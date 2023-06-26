350a8060250d51979e254a9634e8eb75f8064f46
Marilyn.Negrete.-ND <marilyn.negrete.-nd@disney.com>
2023-06-26 14:04:50 -0500

    installed Material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Contacts from "./Pages/Contacts/Contacts";
-
+import Stack from '@mui/material/Stack';
+import Link from '@mui/material/Link';
+import { Button, Box } from "@mui/material";
 
 const App = () => {
     return (
         <>
             <Router>
-                <ul className="navbar-nav mr-auto">
-                    <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                    <li><Link to={'/contacts'} className="nav-link">Contact</Link></li>
-                </ul>
+                <Box display='flex' justifyContent='space-between' sx={{ margin: '2rem 20rem' }}>
+                    <Stack spacing={3} direction="row" alignItems='center' >
+                        <Link href="/" to={'/'} underline="none">
+                            Dashboard
+                        </Link>
+                        <Link href="/contacts" to={'/contacts'} underline="none">
+                            Contacts
+                        </Link>
+                    </Stack>
+                    <Stack direction="row" alignItems='center'>
+                        <Button variant="contained" size="small" disableElevation={true}>Add New Contact</Button>
+                    </Stack>
+                </Box>
                 <Routes>
-                    <Route exact path='/' element={Dashboard} />
-                    <Route path='/contacts' element={Contacts} />
+                    <Route exact path='/' element={<Dashboard />} />
+                    <Route path='/contacts' element={<Contacts />} />
                 </Routes>
             </Router>
         </>
--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,10 +1,61 @@
-import React from "react";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 const Contacts = () => {
     return (
-        <div>
-            Contacts
-        </div>
+        <>
+            <Grid container justifyContent='center'>
+                <Grid item xs={8}>
+                    <Typography sx={{ margin: '2rem 0' }}>All Contacts (4)</Typography>
+                    <List sx={{ margin: '0 auto', bgcolor: 'background.paper' }}>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ImageIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Contact Name" secondary="Jan 9, 2014" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ImageIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Contact Name" secondary="Jan 9, 2014" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ImageIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Contact Name" secondary="Jan 9, 2014" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ImageIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Contact Name" secondary="Jan 9, 2014" />
+                        </ListItem>
+                    </List>
+                </Grid>
+
+            </Grid>
+
+        </>
+
     )
 }
 
